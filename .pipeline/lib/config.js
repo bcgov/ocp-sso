659c4bee837c4b7e9c8763fbae47561bd1f16729
ShellyXueHan <shellshell456@gmail.com>
2019-02-26 11:30:23 -0800

    tagging with phase prefix

--- .pipeline/lib/config.js
+++ .pipeline/lib/config.js
@@ -6,10 +6,10 @@ const name = 'sso'
 
 const phases = {
   build: {namespace:'devops-sso-tools'  , name: `${name}`, phase: 'build', changeId:changeId, suffix: `-build-${changeId}`, instance: `${name}-build-${changeId}`, version:`${version}-${changeId}`, tag:`${version}-${changeId}`},
-   sbox: {namespace:`devops-sso-sandbox`, name: `${name}`, phase: 'sbox' , changeId:changeId, suffix: `-sbox-${changeId}` , instance: `${name}-sbox-${changeId}` , version:`${version}-${changeId}`, tag:`${version}-${changeId}`},
-    dev: {namespace:`devops-sso-dev`    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`${version}-${changeId}`},
-   test: {namespace:`devops-sso-test`   , name: `${name}`, phase: 'test' , changeId:changeId, suffix: '-test'             , instance: `${name}-test`             , version:`${version}-${changeId}`, tag:`${version}`},
-   prod: {namespace:`devops-sso-prod`   , name: `${name}`, phase: 'prod' , changeId:changeId, suffix: ''                  , instance: `${name}-prod`             , version:`${version}-${changeId}`, tag:`${version}`}
+   sbox: {namespace:`devops-sso-sandbox`, name: `${name}`, phase: 'sbox' , changeId:changeId, suffix: `-sbox-${changeId}` , instance: `${name}-sbox-${changeId}` , version:`${version}-${changeId}`, tag:`sbox-${version}-${changeId}`},
+    dev: {namespace:`devops-sso-dev`    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
+   test: {namespace:`devops-sso-test`   , name: `${name}`, phase: 'test' , changeId:changeId, suffix: `-test`             , instance: `${name}-test`             , version:`${version}-${changeId}`, tag:`test-${version}`},
+   prod: {namespace:`devops-sso-prod`   , name: `${name}`, phase: 'prod' , changeId:changeId, suffix: ''                  , instance: `${name}-prod`             , version:`${version}-${changeId}`, tag:`prod-${version}`}
 }
 
 module.exports = exports = phases
