c6c762c2df21c628d8311adb9e0484ada911c8bd
ShellyXueHan <shelly.han@gov.bc.ca>
2019-03-04 15:53:04 -0800

    update pipeline-cli dependency

--- .pipeline/lib/config.js
+++ .pipeline/lib/config.js
@@ -6,10 +6,10 @@ const name = 'sso'
 
 const phases = {
   build: {namespace:'devops-sso-tools'  , name: `${name}`, phase: 'build', changeId:changeId, suffix: `-build-${changeId}`, instance: `${name}-build-${changeId}`, version:`${version}-${changeId}`, tag:`${version}-${changeId}`},
-   sbox: {namespace:`devops-sso-sandbox`, name: `${name}`, phase: 'sbox' , changeId:changeId, suffix: `-sbox-${changeId}` , instance: `${name}-sbox-${changeId}` , version:`${version}-${changeId}`, tag:`sbox-${version}-${changeId}`, host: null},
-    dev: {namespace:`devops-sso-dev`    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`,  host: 'sso-dev.pathfinder.gov.bc.ca'},
-   test: {namespace:`devops-sso-test`   , name: `${name}`, phase: 'test' , changeId:changeId, suffix: `-test`             , instance: `${name}-test`             , version:`${version}-${changeId}`, tag:`test-${version}`,             host: 'sso-test.pathfinder.gov.bc.ca'},
-   prod: {namespace:`devops-sso-prod`   , name: `${name}`, phase: 'prod' , changeId:changeId, suffix: ''                  , instance: `${name}-prod`             , version:`${version}-${changeId}`, tag:`prod-${version}`,             host: 'sso.pathfinder.gov.bc.ca'}
+   sbox: {namespace:'devops-sso-sandbox', name: `${name}`, phase: 'sbox' , changeId:changeId, suffix: `-sbox-${changeId}` , instance: `${name}-sbox-${changeId}` , version:`${version}-${changeId}`, tag:`sbox-${version}-${changeId}`, host: `sso-${changeId}-devops-sso-sandbox.pathfinder.gov.bc.ca`},
+    dev: {namespace:'devops-sso-dev'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`,  host: 'sso-dev.pathfinder.gov.bc.ca'},
+   test: {namespace:'devops-sso-test'   , name: `${name}`, phase: 'test' , changeId:changeId, suffix: `-test`             , instance: `${name}-test`             , version:`${version}-${changeId}`, tag:`test-${version}`,             host: 'sso-test.pathfinder.gov.bc.ca'},
+   prod: {namespace:'devops-sso-prod'   , name: `${name}`, phase: 'prod' , changeId:changeId, suffix: ''                  , instance: `${name}-prod`             , version:`${version}-${changeId}`, tag:`prod-${version}`,             host: 'sso.pathfinder.gov.bc.ca'}
 }
 
 module.exports = exports = phases
