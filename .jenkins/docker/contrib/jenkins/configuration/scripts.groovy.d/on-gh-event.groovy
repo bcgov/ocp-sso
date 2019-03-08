d30b30d0b3f2773fdb5e8bc690f3fd6e76aac67c
ShellyXueHan <36056216+ShellyXueHan@users.noreply.github.com>
2019-03-07 20:37:45 -0800

    Disable clean up for dev (#18)
    * not clean up in dev
    
    * update jenkins pipeline cli version

--- .jenkins/docker/contrib/jenkins/configuration/scripts.groovy.d/on-gh-event.groovy
+++ .jenkins/docker/contrib/jenkins/configuration/scripts.groovy.d/on-gh-event.groovy
@@ -64,7 +64,8 @@ static Map exec(List args, File workingDirectory=null, Appendable stdout=null, A
                     println "Checkout Branch:${sourceBranch}"
 
                     String selector = "env-id=${payload.number},env-name!=prod,github-owner=${repoOwner},github-repo=${repoName},!shared"
-                    ['devops-sso-tools', 'devops-sso-sandbox', 'devops-sso-dev'].each({ namespace ->
+                    // removed dev clean up:
+                    ['devops-sso-tools', 'devops-sso-sandbox'].each({ namespace ->
                         //BuildConfig Output Images
                         def ocGetBcRet = exec(['oc',"--namespace=${namespace}",'get','bc','-l',selector, '-o', 'jsonpath={range .items[*]}{.spec.output.to.namespace}/{.spec.output.to.name}{"\\n"}{end}'])
                         println ocGetBcRet
