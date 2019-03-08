63645f14ec41c59cc6ba4d71f09cd6d16426109a
ShellyXueHan <shelly.han@gov.bc.ca>
2019-03-08 14:47:55 -0800

    change order of two forms

---
+++ extensions/themes/src/main/resources/theme/bcgov-v2/login/resources/js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // kc-form element:
+    const kcForm = document.getElementById('kc-form');
+    // kc-info element:
+    const kcInfo = document.getElementById('kc-info');
+
+    doSwap(kcForm, kcInfo);
+
+});
+
+function doSwap(a, b) {
+  swapElements(document.getElementById(a), document.getElementById(b));
+}
+
+function swapElements(obj1, obj2) {
+  // save the location of obj2
+  var parent2 = obj2.parentNode;
+  var next2 = obj2.nextSibling;
+  // special case for obj1 is the next sibling of obj2
+  if (next2 === obj1) {
+      // just put obj1 before obj2
+      parent2.insertBefore(obj1, obj2);
+  } else {
+      // insert obj2 right before obj1
+      obj1.parentNode.insertBefore(obj2, obj1);
+
+      // now insert obj1 where obj2 was
+      if (next2) {
+          // if there was an element after obj2, then insert obj1 right before that
+          parent2.insertBefore(obj1, next2);
+      } else {
+          // otherwise, just append as last child
+          parent2.appendChild(obj1);
+      }
+  }
+}
