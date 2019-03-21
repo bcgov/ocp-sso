6ff612e992c6dba462872c817a2994124676c64e
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2019-03-20 18:01:39 -0700

    Update images, tag, and version

--- .pipeline/lib/config.js
+++ .pipeline/lib/config.js
@@ -1,7 +1,7 @@
 'use strict';
 const options= require('pipeline-cli').Util.parseArguments()
 const changeId = options.pr //aka pull-request
-const version = '1.1.0'
+const version = '7.3'
 const name = 'sso'
 
 const phases = {
@@ -11,5 +11,10 @@ const phases = {
    test: {namespace:'devops-sso-test'   , name: `${name}`, phase: 'test' , changeId:changeId, suffix: '-test'             , instance: `${name}-test`             , version:`${version}-${changeId}`, tag:`test-${version}`            , host: 'sso-test.pathfinder.gov.bc.ca'},
    prod: {namespace:'devops-sso-prod'   , name: `${name}`, phase: 'prod' , changeId:changeId, suffix: '-prod'             , instance: `${name}-prod`             , version:`${version}-${changeId}`, tag:`prod-${version}`            , host: 'sso.pathfinder.gov.bc.ca'}
 }
+
+process.on('unhandledRejection', (reason) => {
+  console.log(reason);
+  process.exit(1);
+});
 
 module.exports = exports = phases
