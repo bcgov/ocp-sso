eb3c1464f48d7fa2a1d71b53f21f6382d54e39ea
ShellyXueHan <shelly.han@gov.bc.ca>
2019-05-21 15:24:50 -0700

    update theme bcgov-v2

--- extensions/themes/src/main/resources/theme/bcgov-v2/login/resources/js/script.js
+++ extensions/themes/src/main/resources/theme/bcgov-v2/login/resources/js/script.js
@@ -1,21 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    // use gov icon for tab:
     updateFavIcon();
-    // kc-form element:
-    const kcForm = document.getElementById('kc-form');
-    // kc-info element:
-    const kcInfo = document.getElementById('kc-info');
-
-    // Hide kc login form:
-    // kc-form -> kc-form-wrapper -> kc-form-login(hide) & kc-update-profile-form(keep)
-    const kcLoginForm = document.getElementById('kc-form-login');
-    if (kcLoginForm != null) {
-      kcForm.classList.add('hidden');
-    }
-
-    // swap the order of form:
-    if (kcForm && kcInfo) {
-      swapElements(kcForm, kcInfo);
-    }
+    // extent the login message:
+    const errorMsg = document.getElementById('kc-error-message');
+    const titleContent = errorMsg ? 'Log In Error:' : 'Log In With<br/>An Identity Provider:'
+    document.getElementById('kc-page-title').innerHTML = titleContent;
 });
 
 function updateFavIcon() {
@@ -25,26 +14,3 @@ function updateFavIcon() {
   link.href = 'https://portal.nrs.gov.bc.ca/nrs-portal-theme/images/favicon.ico';
   document.getElementsByTagName('head')[0].appendChild(link);
 };
-
-function swapElements(obj1, obj2) {
-  // save the location of obj2
-  var parent2 = obj2.parentNode;
-  var next2 = obj2.nextSibling;
-  // special case for obj1 is the next sibling of obj2
-  if (next2 === obj1) {
-      // just put obj1 before obj2
-      parent2.insertBefore(obj1, obj2);
-  } else {
-      // insert obj2 right before obj1
-      obj1.parentNode.insertBefore(obj2, obj1);
-
-      // now insert obj1 where obj2 was
-      if (next2) {
-          // if there was an element after obj2, then insert obj1 right before that
-          parent2.insertBefore(obj1, next2);
-      } else {
-          // otherwise, just append as last child
-          parent2.appendChild(obj1);
-      }
-  }
-}
