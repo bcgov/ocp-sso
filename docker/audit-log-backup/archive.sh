1e747b2e610434a41f7a54a481f452076ab79991
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2019-11-14 16:50:21 -0800

    Update logs compression/deletion script

--- docker/audit-log-backup/archive.sh
+++ docker/audit-log-backup/archive.sh
@@ -3,17 +3,10 @@ set -Eeuo pipefail
 
 # Setup:
 DIRECTORY="${LOG_PATH:-'/var/log/eap'}"
-# which date of logs to compress:
-LOG_PERIOD="${SSO_LOG_PERIOD:-yesterday}"
 # how far back to expire the logs:
 EXPIRY_LENGTH="${SSO_EXPIRY_LENGTH:-1 month ago}"
 WEBHOOK_URL="${SSO_WEBHOOK_URL:-placeholder}"
 NAMESPACE_NAME="${NAMESPACE_NAME:-sandbox}"
-
-# Get the dates:
-LOG_DATE=$(date -d "${LOG_PERIOD}" '+%Y-%m-%d')
-EXPIRY_DATE=$(date -d "${EXPIRY_LENGTH}" '+%Y-%m-%d')
-echo $LOG_DATE $EXPIRY_DATE
 
 function channelNotification {
   webhookPayload='{"text":"'$NAMESPACE_NAME' - '$1'"}'
@@ -30,25 +23,16 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 cd $DIRECTORY
-# Check for new log files:
-newFiles=$(shopt -s nullglob dotglob; echo *.log.${LOG_DATE})
-if (( ${#newFiles} ))
-then
-  echo "Compressing file for $LOG_DATE"
-  # archive new logs:
-  tar -cvzf ${LOG_DATE}.tar.gz *.log.${LOG_DATE}
-  # delete the log file:
-  rm *.log.${LOG_DATE}
-  # delete expired logs, no error exit:
-  rm ${EXPIRY_DATE}.tar.gz || true
-  rm *.log.${EXPIRY_DATE} || true
-  # send notifiation:
-  webhookMsg="Log backup done for $LOG_DATE"
-  channelNotification "$webhookMsg"
-else
-  # send notifiation:
-  webhookMsg="No new logs for $LOG_DATE"
-  channelNotification "$webhookMsg"
-fi
+# 1) compress old log files into a daily .tar.gz file
+# NOTE: To avoid error message "file changed as we read it", we need to copy the files to a temporary location
+find . -name '*-keycloak.log.*' | grep -hoE '[0-9]+\-[0-9]+\-[0-9]+$' | sort | uniq | xargs -I {} sh -c 'DIR=$(pwd) && mkdir -p "/tmp/{}" && cp *.{} /tmp/{}/ && pushd /tmp/{}/ && tar -czf "$DIR/{}.tar.gz" * && popd && rm -rf "/tmp/{}" && find . -maxdepth 1 -type f -name "*-keycloak.log.{}" -delete'
+# e.g.: List contents -> tar -tvf 2019-11-14.tar.gz
+
+# 2) Delete files that are older than a specific time
+
+EXPIRY_OLDER_THAN=$(date -d "${EXPIRY_LENGTH}" '+%Y%m%d')
+find . -maxdepth 1 -name '*.tar.gz' | xargs -I {} basename -s '.tar.gz' {} | awk -v "ref=${EXPIRY_OLDER_THAN}" '{r=ref +0; b=gensub(/\-/,"", "g", $1) + 0; if (b < r) print $1;}' | xargs -I {} rm -v '{}.tar.gz'
+
+channelNotification "Last Archive 10 Files:\n$(find . -maxdepth 1 -name '*.tar.gz' | sort | tail -n 10 | xargs ls -lah)"
 
 echo 'Done'
