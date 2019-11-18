39eb6d78b1b3b8e8254ace4fd8b8ef8a6942108a
ShellyXueHan <shelly.han@gov.bc.ca>
2019-11-18 10:50:22 -0800

    no errexit

--- docker/audit-log-backup/archive.sh
+++ docker/audit-log-backup/archive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -Eeuo pipefail
+set -Euo pipefail
 
 # Setup:
 DIRECTORY="${LOG_PATH:-'/var/log/eap'}"
@@ -22,16 +22,17 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 cd $DIRECTORY
-# 1) compress old log files into a daily .tar.gz file
+
+# 1) Compress old log files into a daily .tar.gz file
 # NOTE: To avoid error message "file changed as we read it", we need to copy the files to a temporary location
 find . -name '*-keycloak.log.*' | grep -hoE '[0-9]+\-[0-9]+\-[0-9]+$' | sort | uniq | xargs -I {} bash -c 'DIR=$(pwd) && mkdir -p "/tmp/{}" && cp *.{} /tmp/{}/ && pushd /tmp/{}/ && tar -czf "$DIR/{}.tar.gz" * && popd && rm -rf "/tmp/{}" && find . -maxdepth 1 -type f -name "*-keycloak.log.{}" -delete'
 # e.g.: List contents -> tar -tvf 2019-11-14.tar.gz
 
 # 2) Delete files that are older than a specific time
-
 EXPIRY_OLDER_THAN=$(date -d "${EXPIRY_LENGTH}" '+%Y%m%d')
 find . -maxdepth 1 -name '*.tar.gz' | xargs -I {} basename -s '.tar.gz' {} | awk -v "ref=${EXPIRY_OLDER_THAN}" '{r=ref +0; b=gensub(/\-/,"", "g", $1) + 0; if (b < r) print $1;}' | xargs -I {} rm -v '{}.tar.gz'
 
+# Send notification
 channelNotification "Last Archive 10 Files:\n$(find . -maxdepth 1 -name '*.tar.gz' | sort | tail -n 10 | xargs ls -lah)"
 
 echo 'Done'
