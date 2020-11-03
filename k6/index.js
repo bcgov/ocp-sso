781f5e4845a797504554f722a6b270ad8e88efe1
Patrick Simonian <patricksimonian@gmail.com>
2020-11-03 09:31:55 -0800

    add k6 infra

--- k6/index.js
+++ k6/index.js
@@ -1,4 +1,5 @@
 import http from 'k6/http';
+import { parseHTML } from 'k6/html';
 import { sleep } from 'k6';
 
 /**
@@ -7,6 +8,11 @@ import { sleep } from 'k6';
  * 
  */
 export default function () {
-  http.get(`${__ENV.SSO_BASE_URL}`);
+  const res = http.get(`https://developer.gov.bc.ca`);
+  const doc = parseHTML(res.body); // equivalent to res.html()
+  const button = doc.find('button[data-testid="searchbar-button"]').first();
+  console.log('CLICKING')
+  button.click();
+
   sleep(1);
 }
